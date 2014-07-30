a5fe182683076e45ed7ba4272be421f913053caf
Anne Jan Brouwer <ajbrouwer@totalactivemedia.nl>
2014-07-31 00:07:59 +0200

    update

--- mainwindow.h
+++ mainwindow.h
@@ -19,6 +19,9 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
     QDirModel model;
