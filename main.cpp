0867c2128e495eb22bf6fb93365025435e58df6f
Anne Jan Brouwer <brouwer@annejan.com>
2014-07-31 04:09:15 +0200

    check for pass executable

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QProcess>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
     MainWindow w;
-    w.show();
 
-    return app.exec();
+    QProcess *testProcess = new QProcess();
+    if (testProcess->execute("pass") == 0) {
+        w.show();
+        return app.exec();
+    } else {
+        QMessageBox msgBox;
+        msgBox.setText("Please install pass from http://www.passwordstore.org/");
+        msgBox.exec();
+    }
 }
