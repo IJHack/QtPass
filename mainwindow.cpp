899cf2e9b4a7752018b38c1fb39581667ac050ec
Anne Jan Brouwer <ajbrouwer@totalactivemedia.nl>
2014-07-31 18:15:11 +0200

    minor cleanup

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -92,7 +92,7 @@ void MainWindow::on_treeView_clicked(const QModelIndex &index)
  * @param args
  */
 void MainWindow::executePass(QString args) {
-    executeWrapper("pass "+args);
+    executeWrapper(passExecutable + " " + args);
 }
 
 /**
