8554316ec9b8a6a4ea82a03bbe78d1e5d55fe691
Anne Jan Brouwer <brouwer@annejan.com>
2014-08-14 23:14:07 +0200

    search on filePath

--- storemodel.h
+++ storemodel.h
@@ -2,15 +2,20 @@
 #define STOREMODEL_H
 
 #include <QSortFilterProxyModel>
+#include <QFileSystemModel>
 
 class StoreModel : public QSortFilterProxyModel
 {
     Q_OBJECT
+private:
+    QFileSystemModel* fs;
+
 public:
     StoreModel();
 
     bool filterAcceptsRow(int, const QModelIndex &) const;
     bool ShowThis(const QModelIndex) const;
+    void setFSModel(QFileSystemModel *sourceModel);
 };
 
 #endif // STOREMODEL_H
