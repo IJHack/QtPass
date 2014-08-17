ee4bb8d362c8c176fa8a484717c53b484198b7ac
Anne Jan Brouwer <ajbrouwer@totalactivemedia.nl>
2014-08-17 14:43:48 +0200

    nameing fail

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[])
 {
-    SingleApplication app(argc, argv, "testQtPass");
+    SingleApplication app(argc, argv, "QtPass");
     if (app.isRunning()) {
         if (argc == 1 ) {
             app.sendMessage("show");
