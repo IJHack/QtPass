efc258d4e4891eb56020e5bfb90cbfb852e4f192
Anne Jan Brouwer <ajbrouwer@totalactivemedia.nl>
2014-08-22 10:17:14 +0200

    interaction

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,23 @@
-console.log('This would be the main JS file.');
+window.onload = init;
+ 
+function init()
+{
+	var config = document.getElementById('config');
+	var qtpass = document.getElementById('qtpass');
+
+    qtpass.onclick = function()
+    {
+        
+        if (config.classList.contains('hidden')) {
+        	config.classList.remove('hidden');
+        } else {
+        	config.classList.add('hidden');
+        }
+    }
+    
+    config.onclick = function()
+    {
+    	config.classList.add('hidden');
+    }
+}
+ 
