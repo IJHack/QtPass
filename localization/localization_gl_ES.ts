e9458facb34de022e05eb0d05280df9ff7ca2fef
Xose M <brouwer@annejan.com>
2014-12-26 15:01:58 +0100

    Create localization_gl_ES.ts
    galician translation (gl_ES)

---
+++ localization/localization_gl_ES.ts
@@ -0,0 +1,192 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+<name>Dialog</name>
+<message>
+<location filename="../dialog.ui" line="14"/>
+<source>Configuration</source>
+<translation>Preferencias</translation>
+</message>
+<message>
+<location filename="../dialog.ui" line="20"/>
+<source>Pass</source>
+<translation>Pass</translation>
+</message>
+<message>
+<location filename="../dialog.ui" line="26"/>
+<location filename="../dialog.ui" line="72"/>
+<location filename="../dialog.ui" line="79"/>
+<location filename="../dialog.ui" line="135"/>
+<source>...</source>
+<translation type="unfinished"></translation>
+</message>
+<message>
+<location filename="../dialog.ui" line="33"/>
+<source>Executable pass</source>
+<translation>Executable pass</translation>
+</message>
+<message>
+<location filename="../dialog.ui" line="46"/>
+<source>Native</source>
+<translation>Nativo</translation>
+</message>
+<message>
+<location filename="../dialog.ui" line="58"/>
+<source>Executable git</source>
+<translation>Executable git</translation>
+</message>
+<message>
+<location filename="../dialog.ui" line="65"/>
+<source>Executable gpg</source>
+<translation>Executable gpg</translation>
+</message>
+<message>
+<location filename="../dialog.ui" line="91"/>
+<source>Native git/gpg</source>
+<translation>git/gpg nativo</translation>
+</message>
+<message>
+<location filename="../dialog.ui" line="98"/>
+<source>Use pass</source>
+<translation>Usar pass</translation>
+</message>
+<message>
+<location filename="../dialog.ui" line="117"/>
+<source>Other</source>
+<translation>Outros</translation>
+</message>
+<message>
+<location filename="../dialog.ui" line="125"/>
+<source>Folder password-store</source>
+<translation>Cartafol da password-store</translation>
+</message>
+<message>
+<location filename="../dialog.ui" line="146"/>
+<source>Clipboard</source>
+<translation>Portapapeis</translation>
+</message>
+<message>
+<location filename="../dialog.ui" line="153"/>
+<source>Autoclear</source>
+<translation>Limpado automático</translation>
+</message>
+<message>
+<location filename="../dialog.ui" line="160"/>
+<source>Seconds</source>
+<translation>Segundos</translation>
+</message>
+<message>
+<location filename="../dialog.ui" line="170"/>
+<source>Hide password</source>
+<translation>Ocultar contrasinal</translation>
+</message>
+<message>
+<location filename="../dialog.ui" line="177"/>
+<source>Hide content</source>
+<translation>Ocultar contido</translation>
+</message>
+</context>
+<context>
+<name>MainWindow</name>
+<message>
+<location filename="../mainwindow.ui" line="14"/>
+<source>QtPass</source>
+<translation>QtPass</translation>
+</message>
+<message>
+<location filename="../mainwindow.ui" line="23"/>
+<source>Update</source>
+<translation>Actualizar</translation>
+</message>
+<message>
+<location filename="../mainwindow.ui" line="30"/>
+<source>Config</source>
+<translation>Preferencias</translation>
+</message>
+<message>
+<location filename="../mainwindow.ui" line="46"/>
+<source>X</source>
+<translation>X</translation>
+</message>
+<message>
+<location filename="../mainwindow.ui" line="58"/>
+<source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;.Helvetica Neue DeskInterface&apos;; font-size:13pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:16px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:25.6px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Helvetica Neue,Helvetica,Segoe UI,Arial,freesans,sans-serif&apos;; font-size:16px; color:#333333;&quot;&gt;QtPass is a gui for &lt;/span&gt;&lt;a href=&quot;http://www.passwordstore.org/&quot;&gt;&lt;span style=&quot; font-family:&apos;Helvetica Neue,Helvetica,Segoe UI,Arial,freesans,sans-serif&apos;; font-size:16px; text-decoration: underline; color:#4183c4; background-color:transparent;&quot;&gt;pass&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Helvetica Neue,Helvetica,Segoe UI,Arial,freesans,sans-serif&apos;; font-size:x-large; font-weight:600; color:#333333;&quot;&gt;ALPHA release&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Helvetica Neue,Helvetica,Segoe UI,Arial,freesans,sans-serif&apos;; font-size:x-large; font-weight:600; color:#333333;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Helvetica Neue,Helvetica,Segoe UI,Arial,freesans,sans-serif&apos;; font-size:x-large; font-weight:600; color:#333333;&quot;&gt;TODO&lt;/span&gt;&lt;/p&gt;
+&lt;ol style=&quot;margin-top: 0px; margin-bottom: 0px; margin-left: 0px; margin-right: 0px; -qt-list-indent: 1;&quot;&gt;&lt;li style=&quot; font-family:&apos;Helvetica Neue,Helvetica,Segoe UI,Arial,freesans,sans-serif&apos;; font-size:16px; color:#333333;&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:16px;&quot;&gt;edit&lt;/span&gt;&lt;/li&gt;
+&lt;li style=&quot; font-family:&apos;Helvetica Neue,Helvetica,Segoe UI,Arial,freesans,sans-serif&apos;; font-size:16px; color:#333333;&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:16px;&quot;&gt;insert&lt;/span&gt;&lt;/li&gt;
+&lt;li style=&quot; font-family:&apos;Helvetica Neue,Helvetica,Segoe UI,Arial,freesans,sans-serif&apos;; font-size:16px; color:#333333;&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:16px;&quot;&gt;gpg-id management&lt;/span&gt;&lt;/li&gt;&lt;/ol&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Helvetica Neue,Helvetica,Segoe UI,Arial,freesans,sans-serif&apos;; font-size:16px; color:#333333;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:16px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:25.6px;&quot;&gt;&lt;a href=&quot;http://ijhack.github.io/qtpass/&quot;&gt;&lt;span style=&quot; font-family:&apos;Helvetica Neue,Helvetica,Segoe UI,Arial,freesans,sans-serif&apos;; font-size:16px; text-decoration: underline; color:#4183c4; background-color:transparent;&quot;&gt;Documentation&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; line-height:25.6px;&quot;&gt;&lt;a href=&quot;https://github.com/IJHack/qtpass&quot;&gt;&lt;span style=&quot; font-family:&apos;Helvetica Neue,Helvetica,Segoe UI,Arial,freesans,sans-serif&apos;; font-size:16px; text-decoration: underline; color:#4183c4; background-color:transparent;&quot;&gt;Source code&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Helvetica Neue,Helvetica,Segoe UI,Arial,freesans,sans-serif&apos;; color:#333333;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+<translation type="unfinished"></translation>
+</message>
+<message>
+<location filename="../mainwindow.ui" line="83"/>
+<source>qtpass</source>
+<translation>qtpass</translation>
+</message>
+<message>
+<location filename="../mainwindow.cpp" line="166"/>
+<source>Updating password-store</source>
+<translation>Actualizando password-store</translation>
+</message>
+<message>
+<location filename="../mainwindow.cpp" line="257"/>
+<source>Clipboard cleared</source>
+<translation>Portapapeis baleiro</translation>
+</message>
+<message>
+<location filename="../mainwindow.cpp" line="232"/>
+<source>Password copied to clipboard</source>
+<translation>Contrasinal copiado ao portapapeis</translation>
+</message>
+<message>
+<location filename="../mainwindow.cpp" line="242"/>
+<source>Content hidden</source>
+<translation>Contido oculto</translation>
+</message>
+<message>
+<location filename="../mainwindow.cpp" line="293"/>
+<source>QProcess::FailedToStart</source>
+<translation>QProcess::Fallou o inicio</translation>
+</message>
+<message>
+<location filename="../mainwindow.cpp" line="296"/>
+<source>QProcess::Crashed</source>
+<translation>QProcess::Rompeu</translation>
+</message>
+<message>
+<location filename="../mainwindow.cpp" line="299"/>
+<source>QProcess::Timedout</source>
+<translation>QProcess::Caducou</translation>
+</message>
+<message>
+<location filename="../mainwindow.cpp" line="302"/>
+<source>QProcess::ReadError</source>
+<translation>QProcess::Erro de lectura</translation>
+</message>
+<message>
+<location filename="../mainwindow.cpp" line="305"/>
+<source>QProcess::WriteError</source>
+<translation>QProcess::Erro de escritura</translation>
+</message>
+<message>
+<location filename="../mainwindow.cpp" line="308"/>
+<source>QProcess::UnknownError</source>
+<translation>QProcess::Erro descoñecido</translation>
+</message>
+<message>
+<location filename="../mainwindow.cpp" line="354"/>
+<source>Looking for: </source>
+<translation>Buscando: </translation>
+</message>
+</context>
+</TS>
