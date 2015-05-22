3e969e00c4c33adc577cca342a1287cb1e7ef2c7
Anne Jan Brouwer <brouwer@annejan.com>
2015-05-22 04:25:31 +0200

    keygen start, dialog ideas

---
+++ keygendialog.h
@@ -0,0 +1,34 @@
+#ifndef KEYGENDIALOG_H
+#define KEYGENDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class KeygenDialog;
+}
+
+class KeygenDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit KeygenDialog(QWidget *parent = 0);
+    ~KeygenDialog();
+
+private slots:
+    void on_passphrase1_textChanged(const QString &arg1);
+
+    void on_passphrase2_textChanged(const QString &arg1);
+
+    void on_checkBox_stateChanged(int arg1);
+
+    void on_email_textChanged(const QString &arg1);
+
+    void on_name_textChanged(const QString &arg1);
+
+private:
+    Ui::KeygenDialog *ui;
+    void replace(QString, QString);
+};
+
+#endif // KEYGENDIALOG_H
