3cd9eccbafc149157b56341b5c6fe04df8383457
Anne Jan Brouwer <annejan@noprotocol.com>
2015-05-27 04:28:21 +0200

    working profiles, needs some love in dialog

--- dialog.h
+++ dialog.h
@@ -24,6 +24,7 @@ public:
     void setGitPath(QString);
     void setGpgPath(QString);
     void setStorePath(QString);
+    void setProfiles(QHash<QString, QString>, QString);
     void usePass(bool);
     void useClipboard(bool);
     void useAutoclear(bool);
@@ -35,6 +36,7 @@ public:
     QString getGitPath();
     QString getGpgPath();
     QString getStorePath();
+    QHash<QString,QString> getProfiles();
     bool usePass();
     bool useClipboard();
     bool useAutoclear();
