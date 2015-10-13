f52c2540daf1bec36d15244546af2d44da64da24
Anne Jan Brouwer <annejan@noprotocol.com>
2015-10-13 13:37:12 +0200

    optimized assets

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,23 +1,2 @@
-window.onload = init;
- 
-function init()
-{
-	var config = document.getElementById('config');
-	var qtpass = document.getElementById('qtpass');
-
-    qtpass.onclick = function()
-    {
-        
-        if (config.classList.contains('hidden')) {
-        	config.classList.remove('hidden');
-        } else {
-        	config.classList.add('hidden');
-        }
-    }
-    
-    config.onclick = function()
-    {
-    	config.classList.add('hidden');
-    }
-}
- 
+window.onload=init;function init(){var config=document.getElementById('config');var qtpass=document.getElementById('qtpass');qtpass.onclick=function(){if(config.classList.contains('hidden')){config.classList.remove('hidden');}else{config.classList.add('hidden');}}
+config.onclick=function(){config.classList.add('hidden');}}
