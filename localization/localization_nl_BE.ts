04c3d36385055625c3791607e5c16016e43f145d
Anne Jan Brouwer <brouwer@annejan.com>
2016-11-19 22:13:07 +0100

    Windows styling fixes

--- localization/localization_nl_BE.ts
+++ localization/localization_nl_BE.ts
@@ -538,7 +538,7 @@ Expire-Date: 0
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="382"/>
+        <location filename="../mainwindow.ui" line="388"/>
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
 &lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
@@ -602,7 +602,7 @@ p, li { white-space: pre-wrap; }
 &lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="453"/>
+        <location filename="../mainwindow.ui" line="459"/>
         <source>qtpass</source>
         <translation>qtpass</translation>
     </message>
