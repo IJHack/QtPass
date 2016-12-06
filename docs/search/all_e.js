c434ac57c7ee4e4cafce31717209302596fcbd20
Anne Jan Brouwer <brouwer@annejan.com>
2016-12-06 10:05:47 +0100

    Updated docs

--- docs/search/all_e.js
+++ docs/search/all_e.js
@@ -6,6 +6,5 @@ var searchData=
   ['realpass_2eh',['realpass.h',['../realpass_8h.html',1,'']]],
   ['receivemessage',['receiveMessage',['../class_single_application.html#a3257ffeb5c48de33e6ded14902c9f238',1,'SingleApplication']]],
   ['reencryptpath',['reencryptPath',['../class_imitate_pass.html#a8670f6326e3dfe0018a0737fba27da2f',1,'ImitatePass']]],
-  ['remove',['Remove',['../class_imitate_pass.html#ab9cef44c64dae297da55e08266f1c1c5',1,'ImitatePass::Remove()'],['../class_pass.html#a5aedb6df7aa57385eb19a78cdbd3e764',1,'Pass::Remove()'],['../class_real_pass.html#a96679194b4a0e6b99eb41cf25349c7fd',1,'RealPass::Remove()']]],
-  ['resetpasswordstoredir',['resetPasswordStoreDir',['../class_pass.html#a5377c479d9fb9c32ab7e218392b903cf',1,'Pass']]]
+  ['remove',['Remove',['../class_imitate_pass.html#ab9cef44c64dae297da55e08266f1c1c5',1,'ImitatePass::Remove()'],['../class_pass.html#a5aedb6df7aa57385eb19a78cdbd3e764',1,'Pass::Remove()'],['../class_real_pass.html#a96679194b4a0e6b99eb41cf25349c7fd',1,'RealPass::Remove()']]]
 ];
