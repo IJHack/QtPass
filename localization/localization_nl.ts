6bb90a06ab6034d9ad24c94ad9a1b0f2ec2e26c6
Erik Quaeghebeur <qtpass@equaeghe.nospammail.net>
2018-09-12 23:06:54 +0200

    Unify nl_BE and nl_NL localization files
    There was essentially no difference between these two variants.
    So it would be a hassle to maintain both separately.
    The difference there was (Seconden vs. Secondes) is not even
    one that is a substantive difference between BE and NL. Both
    are used in both regions and both are correct.

--- localization/localization_nl.ts
+++ localization/localization_nl.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="nl_NL">
+<TS version="2.1" language="nl">
 <context>
     <name>ConfigDialog</name>
     <message>
@@ -31,7 +31,7 @@
     <message>
         <location/>
         <source>Seconds</source>
-        <translation>Secondes</translation>
+        <translation>Seconden</translation>
     </message>
     <message>
         <location/>
