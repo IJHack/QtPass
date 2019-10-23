0261dd622e2e03b469e83258f4b4fa513338861d
Anne Jan Brouwer <brouwer@annejan.com>
2019-10-23 15:40:42 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (201 of 201 strings)
    
    Translation: QtPass/QtPass
    Translate-URL: https://hosted.weblate.org/projects/qtpass/qtpass/nl/

--- localization/localization_nl_NL.ts
+++ localization/localization_nl_NL.ts
@@ -89,7 +89,7 @@
     </message>
     <message>
         <source>Current path</source>
-        <translation type="unfinished"></translation>
+        <translation>Huidige pad</translation>
     </message>
     <message>
         <source>Use pwgen</source>
@@ -341,49 +341,51 @@ email</translation>
     </message>
     <message>
         <source>Use PWGen</source>
-        <translation type="unfinished"></translation>
+        <translation>Gebruik PWGen</translation>
     </message>
     <message>
         <source>Use Git</source>
-        <translation type="unfinished"></translation>
+        <translation>Gebruik Git</translation>
     </message>
     <message>
         <source>Use QRencode</source>
-        <translation type="unfinished"></translation>
+        <translation>Gebruik QRencode</translation>
     </message>
     <message>
         <source>Use pass-otp extension</source>
-        <translation type="unfinished"></translation>
+        <translation>Gebruik de pass-otp extensie</translation>
     </message>
     <message>
         <source>Nati&amp;ve Git/GPG</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Systeemeigen Git/GPG</translation>
     </message>
     <message>
         <source>Git</source>
-        <translation type="unfinished"></translation>
+        <translation>Git</translation>
     </message>
     <message>
         <source>…</source>
-        <translation type="unfinished"></translation>
+        <translation>…</translation>
     </message>
     <message>
         <source>GPG</source>
-        <translation type="unfinished"></translation>
+        <translation>GPG</translation>
     </message>
     <message>
         <source>PWGen</source>
-        <translation type="unfinished"></translation>
+        <translation>PWGen</translation>
     </message>
     <message>
         <source>Templates add extra fields in the password generation dialogue, and in the password view.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sjablonen voegen extra velden toe in de dialoog voor het genereren van wachtwoorden en in de wachtwoordweergave.</translation>
     </message>
     <message>
         <source>login
 URL
 e-mail</source>
-        <translation type="unfinished"></translation>
+        <translation>login
+URL
+e-mail</translation>
     </message>
 </context>
 <context>
