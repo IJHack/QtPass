53519d8ce7d0195c7aed2d0f0736f09e26588a64
Anne Jan Brouwer <brouwer@annejan.com>
2020-01-07 13:26:54 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (189 of 189 strings)
    
    Translation: QtPass/QtPass
    Translate-URL: https://hosted.weblate.org/projects/qtpass/qtpass/nl/

--- localization/localization_nl_NL.ts
+++ localization/localization_nl_NL.ts
@@ -111,7 +111,7 @@
     <message>
         <location filename="../src/configdialog.ui" line="411"/>
         <source>Include special symbols</source>
-        <translation type="unfinished"></translation>
+        <translation>Gebruik speciale symbolen</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="915"/>
