25312b2183c6184a39608426f381703b4576ae9a
Anne Jan Brouwer <brouwer@annejan.com>
2020-01-29 12:07:40 +0100

    Cleaner

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,5 @@
 window.onload=init;function init(){var config=document.getElementById('config');var qtpass=document.getElementById('qtpass');qtpass.onclick=function(){if(config.classList.contains('hidden')){config.classList.remove('hidden');}else{config.classList.add('hidden');}}
 config.onclick=function(){config.classList.add('hidden');}}
 if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then(function() {
-      console.log('Service Worker Registered');
-    });
+  navigator.serviceWorker.register('/sw.js');
 }
