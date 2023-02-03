55c01ea841e20b170843681e132ef8fb386de941
josé m <correoxm@disroot.org>
2023-02-03 05:15:40 +0000

    Translated using Weblate (Galician)
    Currently translated at 100.0% (192 of 192 strings)
    
    Translation: QtPass/QtPass
    Translate-URL: https://hosted.weblate.org/projects/qtpass/qtpass/gl/

--- localization/localization_gl_ES.ts
+++ localization/localization_gl_ES.ts
@@ -136,17 +136,17 @@
     <message>
         <location filename="../src/configdialog.ui" line="220"/>
         <source>Use a monospace font</source>
-        <translation type="unfinished"></translation>
+        <translation>Usar letra monoespazo</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="227"/>
         <source>Display the files content as-is</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar o contido do ficheiro tal como é</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="234"/>
         <source>No line wrapping</source>
-        <translation type="unfinished"></translation>
+        <translation>Sen axustes de liña</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="456"/>
