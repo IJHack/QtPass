b12a0a688ae69433c30484fa25dd60473cd6c7b7
Сергей <asvmail.as@gmail.com>
2023-04-01 13:30:46 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (192 of 192 strings)
    
    Translation: QtPass/QtPass
    Translate-URL: https://hosted.weblate.org/projects/qtpass/qtpass/ru/

--- localization/localization_ru_RU.ts
+++ localization/localization_ru_RU.ts
@@ -73,17 +73,17 @@
     <message>
         <location filename="../src/configdialog.ui" line="220"/>
         <source>Use a monospace font</source>
-        <translation type="unfinished"></translation>
+        <translation>Использовать моноширинный шрифт</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="227"/>
         <source>Display the files content as-is</source>
-        <translation type="unfinished"></translation>
+        <translation>Отображать содержимое файлов «как есть»</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="234"/>
         <source>No line wrapping</source>
-        <translation type="unfinished"></translation>
+        <translation>Нет переноса строк</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="266"/>
