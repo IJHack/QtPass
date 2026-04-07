910d7940f75cca4f7c3f5ae71ee746e409ba9bfd
Henk <henk@annejan.com>
2026-04-07 21:25:09 +0200

    fix: use subshell for cd to avoid leaving wrong directory

--- scripts/release-linux.sh
+++ scripts/release-linux.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+(
+	cd artwork
+	cp icon.png qtpass-icon.png
+	xdg-icon-resource install --size 64 qtpass-icon.png
+)
+
 qmake CONFIG+=release && make && sudo make install
-cd artwork
-cp icon.png qtpass-icon.png
-xdg-icon-resource install --size 64 qtpass-icon.png 
-cd ..
 sudo cp qtpass.desktop /usr/share/applications/
