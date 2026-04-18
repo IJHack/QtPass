9144a764ad4298b0aaabd3ebf2e0c55f68529b8a
Anonymous <noreply@weblate.org>
2026-04-18 23:29:19 +0200

    Translated using Weblate (Sinhala)
    Currently translated at 8.1% (19 of 232 strings)
    
    Translation: QtPass/QtPass
    Translate-URL: https://hosted.weblate.org/projects/qtpass/qtpass/si/

--- localization/localization_si_LK.ts
+++ localization/localization_si_LK.ts
@@ -896,7 +896,7 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="../src/mainwindow.ui" line="452"/>
         <source>Configuration</source>
-        <translation type="unfinished">වින්‍යාසය</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="449"/>
@@ -1374,7 +1374,7 @@ Continue?</source>
     <message>
         <location filename="../src/passworddialog.ui" line="129"/>
         <source>Custom</source>
-        <translation type="unfinished">අභිරුචි</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/passworddialog.ui" line="143"/>
