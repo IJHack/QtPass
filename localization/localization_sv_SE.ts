e62f3e4103c2c17677f20be4700bee55cb0605f3
Anne Jan Brouwer <brouwer@annejan.com>
2026-04-19 16:40:19 +0200

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (255 of 255 strings)
    
    Translation: QtPass/QtPass
    Translate-URL: https://hosted.weblate.org/projects/qtpass/qtpass/sv/

--- localization/localization_sv_SE.ts
+++ localization/localization_sv_SE.ts
@@ -1060,7 +1060,7 @@ Expire-Date: 0
     <message>
         <location filename="../src/mainwindow.ui" line="152"/>
         <source>Case-insensitive toggle</source>
-        <translation>Skiftlägesokänsligt växel</translation>
+        <translation>Skiftlägesokänslig växel</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="155"/>
