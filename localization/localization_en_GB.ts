7a5798c8b83d3c240c77c0fb36efdb39509968d5
Anne Jan Brouwer <brouwer@annejan.com>
2026-04-28 16:41:17 +0200

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (304 of 304 strings)
    
    Translation: QtPass/QtPass
    Translate-URL: https://hosted.weblate.org/projects/qtpass/qtpass/en_GB/

--- localization/localization_en_GB.ts
+++ localization/localization_en_GB.ts
@@ -489,7 +489,7 @@ email</translation>
     <message>
         <location filename="../src/configdialog.cpp" line="1272"/>
         <source>Fill in all required fields</source>
-        <translation>Fill in all required fields.</translation>
+        <translation>Fill in all required fields</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="846"/>
