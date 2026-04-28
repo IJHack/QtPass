c3c5ce9c8eaf84c7e3498ebb880ebe93431a7c0b
Anne Jan Brouwer <brouwer@annejan.com>
2026-04-28 16:06:52 +0200

    Translated using Weblate (Afrikaans)
    Currently translated at 100.0% (304 of 304 strings)
    
    Translation: QtPass/QtPass
    Translate-URL: https://hosted.weblate.org/projects/qtpass/qtpass/af/

--- localization/localization_af_ZA.ts
+++ localization/localization_af_ZA.ts
@@ -489,7 +489,7 @@ email</translation>
     <message>
         <location filename="../src/configdialog.cpp" line="1272"/>
         <source>Fill in all required fields</source>
-        <translation>Vul al die vereiste velde in.</translation>
+        <translation>Vul al die vereiste velde in</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="846"/>
