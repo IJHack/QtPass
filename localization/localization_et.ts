10f2484f7b2dc22484f07d81f9631256b6390f47
Weblate (bot) <noreply@weblate.org>
2026-05-01 23:45:26 +0200

    Translated using Weblate (Estonian) (#1410)
    Currently translated at 100.0% (304 of 304 strings)
    
    Translation: QtPass/QtPass
    Translate-URL: https://hosted.weblate.org/projects/qtpass/qtpass/et/
    
    Co-authored-by: Priit Jõerüüt <jrthwlate@users.noreply.hosted.weblate.org>

--- localization/localization_et.ts
+++ localization/localization_et.ts
@@ -126,7 +126,7 @@
     <message>
         <location filename="../src/configdialog.ui" line="426"/>
         <source>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789</source>
-        <translation type="unfinished">ABCDEFGHIJKLMNOPQRSTUVWXYZŠŽÄÖÕÜabcdefghijklmnopqrstuvwxyzšžäöõü0123456789</translation>
+        <translation>ABCDEFGHIJKLMNOPQRSTUVWXYZŠŽÄÖÕÜabcdefghijklmnopqrstuvwxyzšžäöõü0123456789</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="460"/>
@@ -519,7 +519,7 @@ email</translation>
     <message>
         <location filename="../src/configdialog.cpp" line="914"/>
         <source>Please install GnuPG on your system.&lt;br&gt;Install &lt;strong&gt;Ubuntu&lt;/strong&gt; from the Microsoft Store to get it.&lt;br&gt;If you already did so, make sure you started it once and&lt;br&gt;click &quot;Autodetect&quot; in the next dialog.</source>
-        <translation type="unfinished">Palun paigalda oma arvutisse GnuPG.&lt;br&gt;Selleks lisa Microsoft Store&apos;ist &lt;strong&gt;Ubuntu&lt;/strong&gt;. Kui oled seda juba teinud, siis kontrolli, et oled ta vähemalt korra käivitanud ja&lt;br&gt;klõpsi järgmises vaates „Tuvasta automaatselt“.</translation>
+        <translation>Palun paigalda oma arvutisse GnuPG.&lt;br&gt;Selleks lisa Microsoft Store&apos;ist &lt;strong&gt;Ubuntu&lt;/strong&gt;. Kui oled seda juba teinud, siis kontrolli, et oled ta vähemalt korra käivitanud ja&lt;br&gt;klõpsi järgmises vaates „Tuvasta automaatselt“.</translation>
     </message>
     <message>
         <location filename="../src/configdialog.cpp" line="919"/>
