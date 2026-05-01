905c26658f7dba27ac659a60c7a2762299b76066
Anne Jan Brouwer <brouwer@annejan.com>
2026-05-01 21:57:09 +0200

    i18n: extend password charset for Slavic Latin + other diacritic locales
    Same extend-don't-replace principle as the previous commit. ASCII
    preserved; locale-specific letters appended for users who want
    passwords with native characters.
    
    Slavic Latin:
    - pl: + ĄĆĘŁŃÓŚŹŻ ąćęłńóśźż
    - cs: + ÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ áčďéěíňóřšťúůýž
    - sk: + ÁÄČĎÉÍĹĽŇÓÔŔŠŤÚÝŽ áäčďéíĺľňóôŕšťúýž
    - hr: + ČĆĐŠŽ čćđšž
    - sr_RS (Latin Serbian): + ČĆĐŠŽ čćđšž
    - sl: + ČŠŽ čšž
    
    Other Latin diacritic:
    - hu: + ÁÉÍÓÖŐÚÜŰ áéíóöőúüű
    - ro: + ĂÂÎȘȚ ăâîșț
    - sq: + ÇË çë
    - tr: + ÇĞİÖŞÜ çğıöşü (note: Turkish lowercase ı is dotless, distinct
      from ASCII i; ASCII I/i kept too)
    - et: + ŠŽÄÖÕÜ šžäöõü
    - lv: + ĀČĒĢĪĶĻŅŠŪŽ āčēģīķļņšūž
    - lt: + ĄČĘĖĮŠŲŪŽ ąčęėįšųūž
    
    All flagged type="unfinished" so Weblate native reviewers can
    adjust per locale convention.

--- localization/localization_hr.ts
+++ localization/localization_hr.ts
@@ -126,7 +126,7 @@
     <message>
         <location filename="../src/configdialog.ui" line="426"/>
         <source>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789</source>
-        <translation>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789</translation>
+        <translation type="unfinished">ABCDEFGHIJKLMNOPQRSTUVWXYZČĆĐŠŽabcdefghijklmnopqrstuvwxyzčćđšž0123456789</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="460"/>
--- localization/localization_lv.ts
+++ localization/localization_lv.ts
@@ -122,7 +122,7 @@
     <message>
         <location filename="../src/configdialog.ui" line="426"/>
         <source>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789</source>
-        <translation>ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789</translation>
+        <translation type="unfinished">ABCDEFGHIJKLMNOPQRSTUVWXYZĀČĒĢĪĶĻŅŠŪŽabcdefghijklmnopqrstuvwxyzāčēģīķļņšūž0123456789</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="443"/>
