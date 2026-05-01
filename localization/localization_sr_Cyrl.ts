059053a27d062ec89401216fc40c5aeccd7d455a
Anne Jan Brouwer <brouwer@annejan.com>
2026-05-02 00:14:17 +0200

    i18n(sr_Cyrl): add missing <br> in install hint (MS Store/GnuPG.org)
    Source has 2 <br> tags; translation had only 1 (after the first
    sentence). Missing the <br> between "Microsoft Store" and "или"
    — so the second-line "or download from GnuPG.org" alternative
    ran together with the Microsoft Store sentence.
    
    Inserted <br> at the appropriate position. Other content
    preserved.

--- localization/localization_sr_Cyrl.ts
+++ localization/localization_sr_Cyrl.ts
@@ -524,7 +524,7 @@ email</translation>
     <message>
         <location filename="../src/configdialog.cpp" line="919"/>
         <source>Please install GnuPG on your system.&lt;br&gt;Install &lt;strong&gt;Ubuntu&lt;/strong&gt; from the Microsoft Store&lt;br&gt;or &lt;a href=&quot;https://www.gnupg.org/download/#sec-1-2&quot;&gt;download&lt;/a&gt; it from GnuPG.org</source>
-        <translation>Молимо, поставете GnuPG на вашу рачунарску опрему.&lt;br&gt;Установите &lt;strong&gt;Ubuntu&lt;/strong&gt; са Microsoft Store или преузми га са &lt;a href=&quot;https://www.gnupg.org/download/#sec-1-2&quot;&gt;GnuPG.org&lt;/a&gt;</translation>
+        <translation>Молимо, поставете GnuPG на вашу рачунарску опрему.&lt;br&gt;Установите &lt;strong&gt;Ubuntu&lt;/strong&gt; са Microsoft Store&lt;br&gt;или преузми га са &lt;a href=&quot;https://www.gnupg.org/download/#sec-1-2&quot;&gt;GnuPG.org&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="443"/>
