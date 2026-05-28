f75dcd31d9b36979d4829cdb9b6dba3b993e264e
Anne Jan Brouwer <brouwer@annejan.com>
2026-05-28 08:59:07 +0200

    Translated using Weblate (Dutch (Belgium))
    Currently translated at 100.0% (314 of 314 strings)
    
    Translation: QtPass/QtPass
    Translate-URL: https://hosted.weblate.org/projects/qtpass/qtpass/nl_BE/

--- localization/localization_nl_BE.ts
+++ localization/localization_nl_BE.ts
@@ -1564,12 +1564,12 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="../src/mainwindow.cpp" line="1014"/>
         <source>Invalid name</source>
-        <translation type="unfinished">Ongeldige naam</translation>
+        <translation>Ongeldige naam</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="1015"/>
         <source>That name would resolve outside the password store. Please choose a different name.</source>
-        <translation type="unfinished"></translation>
+        <translation>Die naam werkt niet binnen het wachtwoordsysteem. Kies een andere naam.</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="1425"/>
