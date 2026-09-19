f445dcdeac6a1ac239a8f6b1546dceaf3b2491e5
Anonymous <noreply@weblate.org>
2026-09-19 16:06:20 +0200

    Translated using Weblate (Catalan)
    Currently translated at 62.8% (278 of 442 strings)
    
    Translation: QtPass/QtPass
    Translate-URL: https://hosted.weblate.org/projects/qtpass/qtpass/ca/

--- localization/localization_ca.ts
+++ localization/localization_ca.ts
@@ -155,17 +155,17 @@
     <message>
         <location filename="../src/configdialog.ui" line="60"/>
         <source>General</source>
-        <translation type="unfinished">General</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="65"/>
         <source>Clipboard</source>
-        <translation type="unfinished">Porta-retalls</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="70"/>
         <source>Passwords</source>
-        <translation type="unfinished">Contrasenyes</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/configdialog.ui" line="173"/>
@@ -941,17 +941,17 @@ e-mail</translation>
     <message>
         <location filename="../src/fieldlabel.cpp" line="13"/>
         <source>Double-click to rename this field</source>
-        <translation type="unfinished">Feu doble clic per canviar el nom d&apos;aquest camp</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/fieldlabel.cpp" line="26"/>
         <source>Rename field…</source>
-        <translation type="unfinished">Canvia el nom del camp…</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/fieldlabel.cpp" line="27"/>
         <source>Remove field</source>
-        <translation type="unfinished">Suprimeix el camp</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -2490,12 +2490,12 @@ Voleu continuar?</translation>
     <message>
         <location filename="../src/passworddialog.cpp" line="329"/>
         <source>Remove field</source>
-        <translation type="unfinished">Suprimeix el camp</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/passworddialog.cpp" line="349"/>
         <source>A field called %1 already exists.</source>
-        <translation type="unfinished">Ja existeix un camp anomenat %1.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/passworddialog.cpp" line="420"/>
